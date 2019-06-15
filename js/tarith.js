25edbae5c965319bbb3888a5e5047c49adbe0e83
Kirill <Semennikov1360@mail.ru>
2019-06-15 15:05:11 +0300

    Bug fix

--- js/tarith.js
+++ js/tarith.js
@@ -2,12 +2,20 @@ var people;
 var  diskSpace;
 var support;
 var typeDisk;
+var mount;
 var price=document.querySelector("#price");
 function price1() {
    people=document.querySelector("#people").value;
    diskSpace=document.querySelector("#diskSpace").value;
    support=document.querySelector("#support").value;
    typeDisk=document.querySelector("#typeDisk").value;
+   mount = document.querySelector("#mounth").value;
+   if(mount >= 12){
+       document.querySelector("#present").innerHTML = "Домен в подарок";
+   }
+   else {
+       document.querySelector("#present").innerHTML = "";
+   }
    var pricet=people*typeDisk*(diskSpace+support);
     console.log( );
     price.innerHTML = "Цена: "+pricet+"$";
